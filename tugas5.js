1b772545b2756886be3d9e8e31f32f186e8f14ab
Darma Riduan <dharmarhiduwhan@gmail.com>
2020-01-15 15:19:55 +0700

    tugas 5 js

---
+++ tugas5.js
@@ -0,0 +1,19 @@
+//
+// // Perulangan For
+// var i = 3;
+// while (i<20) {
+//   console.log(i);
+//   i+=3;
+// }
+//
+// // Perulangan While
+// for (var i = 3; i < 20; i+=3) {
+//   console.log(i);
+// }
+
+// Perulangan Do While
+var i = 3;
+do {
+  console.log(i);
+  i+=3;
+} while (i<20);
